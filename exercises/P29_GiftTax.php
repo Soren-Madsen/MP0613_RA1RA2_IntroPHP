7e9e632a286b8efa08bab493c6cdbf10a06b82fe
Soren-Madsen <sorenmadsen13@gmail.com>
2025-09-23 16:49:26 +0200

    ex 29

--- exercises/P29_GiftTax.php
+++ exercises/P29_GiftTax.php
@@ -4,8 +4,33 @@ class P29_GiftTax
 {
     public function main(): void
     {
+        $input = trim(string: fgets($GLOBALS['STDIN'] ?? STDIN));
+        $input = (int)$input;
+        function calculateGiftTax($input)
+        {
+            if ($input < 5000) {
+                return 0;
+            } elseif ($input <= 25000) {
+                return 100 + ($input - 5000) * 0.08;
+            } elseif ($input <= 55000) {
+                return 1700 + ($input - 25000) * 0.10;
+            } elseif ($input <= 200000) {
+                return 4700 + ($input - 55000) * 0.12;
+            } elseif ($input <= 1000000) {
+                return 22100 + ($input - 200000) * 0.15;
+            } else {
+                return 142100 + ($input - 1000000) * 0.17;
+            }
+        }
         // Write your code here
-        echo "Value of the gift?";
-       
+        echo "Value of the gift? ";
+        
+        $tax = calculateGiftTax($input);
+        
+        if ($tax == 0) {
+            echo "No tax!\n";
+        } else {
+            echo "Tax: " . $tax . "\n";
+        }
     }
 }
